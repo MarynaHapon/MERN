86267b218790874dff2783f0667c56597d676ba0
Maryna Hapon <marynahapon@gmail.com>
2019-05-17 23:24:27 +0300

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -152,4 +152,23 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+/*
+    @route  DELETE api/profile
+    @desc   Delete profile, user and posts
+    @access Private
+*/
+router.delete("/", auth, async (req, res) => {
+  try {
+    /*
+      @todo - remove users posts
+    */
+    await Profile.findOneAndRemove({ user: req.user.id });
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json("Server Error");
+  }
+});
+
 module.exports = router;
